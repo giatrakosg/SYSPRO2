a44bf1db23b6b247df813c3db1bba61b28fdc9b2
loney <geo.giatrakos@gmail.com>
2019-04-09 20:17:10 +0300

    Implement bytes counting

--- get_stats.sh
+++ get_stats.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+echo -n "Bytes Received "
+echo `cat *.log | grep bytes_received | sed 's/[^0-9]*//g' | tr '\n' ' ' | tr ' ' '+' ` "0" | bc
+echo -n "Bytes Sent "
+echo `cat *.log | grep bytes_sent | sed 's/[^0-9]*//g' | tr '\n' ' ' | tr ' ' '+' ` "0" | bc
